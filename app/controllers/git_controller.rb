c2793f66b6a5163ab33a2eaac6cf7d398c263aea
Jae Ho Jeon <deity@snu.ac.kr>
2014-11-07 16:17:35 +0900

    cleaned up git code

--- app/controllers/git_controller.rb
+++ app/controllers/git_controller.rb
@@ -1,21 +1,16 @@
-FolderPath = "/Users/deity/programming/graphics_hw"
-FilePath = 'Animation/Animation/main.cpp'
+FolderPath = "/Users/deity/programming/hcil_rails"
+FilePath = 'app/admin/person.rb'
 
 class GitController < ApplicationController
   def index
     repo = Gitlab::Git::Repository.new(FolderPath)
-    @blob = Gitlab::Git::Blob.find(repo, 'master', FilePath)
-    logger.info @blob.inspect
-
     commits = Gitlab::Git::Commit.where({
       repo: repo,
       ref: 'master',
       limit:1000,
-      path: 'Animation/Animation/main.cpp'
-    }).find_all { |c| Gitlab::Git::Blob.find(repo, c.id, FilePath) }
+      path: FilePath
+    })
 
     @blobs = commits.map { |c| Gitlab::Git::Blob.find(repo, c.id, FilePath) }.reverse
-
-
   end
 end
